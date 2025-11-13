0a90ad089692bae197a7d87831becd7773e9d6d8
oliullahakib <oliullahakib@gmail.com>
2025-11-13 09:59:46 +0600

    made financial summary conditional in report page

--- src/Pages/Reports.jsx
+++ src/Pages/Reports.jsx
@@ -3,11 +3,13 @@ import Mydiv from '../component/Mydiv';
 import { AuthContext } from '../component/Context/AuthContext';
 import useAxiosSecure from '../hook/useAxiosSecure';
 import { Bar, BarChart,Tooltip, XAxis, YAxis } from 'recharts';
+import { Link, useLocation } from 'react-router';
 
 const Reports = () => {
      const axiosSecure = useAxiosSecure()
      const [transactions, setTransactions] = useState([])
      const { user } = use(AuthContext)
+     const location=useLocation()
      useEffect(() => {
             axiosSecure.get(`/my-transactions?email=${user.email}`)
                 .then(data => {
@@ -23,17 +25,23 @@ const Reports = () => {
         const data = Object.keys(dataObj).map(category=>({category,total:dataObj[category]}))
     return (
         <div>
-            <Mydiv className='my-10 flex flex-col justify-center'>
+            {
+                data.length===0?<div className='flex flex-col items-center justify-center min-h-96'>
+                    <h1 className="text-2xl md:text-5xl momo-font linear-text my-5 font-bold text-center"> Summary Not Available </h1>
+                     <Link state={location.pathname} className='btn primary-btn w-40 mx-3' to={"/add-transaction"}>Add A Transaction </Link>
+                </div>
+                :<Mydiv className='my-10 px-3 flex flex-col justify-center'>
                 <h1 className="text-3xl md:text-5xl momo-font linear-text my-5 font-bold text-center"> Financial Summary </h1>
                 <div className='flex justify-center mt-5'>
                     <BarChart width={800} height={500} data={data}>
-                    <Bar width={100} fill='#28D99D' dataKey={"total"}/>
+                    <Bar barSize={50} fill='#28D99D' dataKey={"total"}/>
                     <XAxis dataKey={'category'} />
                     <YAxis/>
                     <Tooltip contentStyle={{backgroundColor:'black',border:'black'}} />
                 </BarChart>
                 </div>
-            </Mydiv>   
+            </Mydiv> 
+            }  
         </div>
     );
 };
